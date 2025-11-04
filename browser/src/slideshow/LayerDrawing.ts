70e45e351cd34d92ef556465031bc67b040ac212
Rashesh Padia <rashesh.padia@collabora.com>
2025-11-04 15:48:52 +0530

    slideshow: fix js error
    ```
    TypeError: Cannot read properties of null (reading 'index')
        at LayerDrawing.onSlideRenderingComplete (file:///home/rashesh/collabora/coda/online/browser/dist/src/slideshow/LayerDrawing.js:651:91)
        at Evented.fire (file:///home/rashesh/collabora/coda/online/browser/dist/src/app/Events.js:265:34)
        at NewClass.fire (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Events.js:20:23)
        at app.definitions.Socket.Socket._onMessage (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:1172:27)
        at app.definitions.Socket.Socket._emitSlurpedEvents (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:346:30)
        at file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:299:22
    js: Exception TypeError: Cannot read properties of null (reading 'index') emitting event sliderenderingcomplete: success
    TypeError: Cannot read properties of null (reading 'index')
        at LayerDrawing.onSlideRenderingComplete (file:///home/rashesh/collabora/coda/online/browser/dist/src/slideshow/LayerDrawing.js:651:91)
        at Evented.fire (file:///home/rashesh/collabora/coda/online/browser/dist/src/app/Events.js:265:34)
        at NewClass.fire (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Events.js:20:23)
        at app.definitions.Socket.Socket._onMessage (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:1172:27)
        at app.definitions.Socket.Socket._emitSlurpedEvents (file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:346:30)
        at file:///home/rashesh/collabora/coda/online/browser/dist/src/core/Socket.js:299:22
    ```
    
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: I40e2663c0cd7e15ce68d4f26a689b8135d9423fe

--- browser/src/slideshow/LayerDrawing.ts
+++ browser/src/slideshow/LayerDrawing.ts
@@ -830,20 +830,13 @@ class LayerDrawing {
 		this.map.fire('handleslideshowprogressbar', { isVisible: false });
 
 		// show welcome slideshow once 1st slide is rendered
-		const index = this.getSlideInfo(
-			this.requestedSlideHash || this.prefetchedSlideHash,
-		).index;
-		if (
-			app.map.slideShowPresenter._isWelcomePresentation &&
-			index === 0 &&
-			window.mode.isCODesktop()
-		)
+		const slideHash = this.requestedSlideHash || this.prefetchedSlideHash;
+		const slideInfo = this.getSlideInfo(slideHash);
+		const index = slideInfo ? slideInfo.index : undefined;
+		if (app.map.slideShowPresenter._isWelcomePresentation && index === 0 && window.mode.isCODesktop())
 			app.socket.sendMessage('SHOW_WELCOME');
 
 		if (!e.success) {
-			const slideHash = this.requestedSlideHash || this.prefetchedSlideHash;
-			const slideInfo = this.getSlideInfo(slideHash);
-			const index = slideInfo ? slideInfo.index : undefined;
 			this.requestedSlideHash = null;
 			this.prefetchedSlideHash = null;
 			app.console.debug(
@@ -873,7 +866,7 @@ class LayerDrawing {
 		this.cacheAndNotify();
 
 		// fetch next slide and draw it on offscreen canvas
-		if (reqSlideInfo.next && !this.slideCache.has(reqSlideInfo.next)) {
+		if (reqSlideInfo?.next && !this.slideCache.has(reqSlideInfo.next)) {
 			this.requestSlideImpl(reqSlideInfo.next, true);
 		}
 	}
