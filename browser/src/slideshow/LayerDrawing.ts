3736e74fd4c3eae63e24b7271eaac31ef26c15f7
Rashesh Padia <rashesh.padia@collabora.com>
2025-11-09 19:35:37 +0530

    CODA: show placeholder svg until welcomeslideshow is loaded
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: If7fa172d48aa90e0442a388c2f2deac591df523b

--- browser/src/slideshow/LayerDrawing.ts
+++ browser/src/slideshow/LayerDrawing.ts
@@ -833,8 +833,22 @@ class LayerDrawing {
 		const slideHash = this.requestedSlideHash || this.prefetchedSlideHash;
 		const slideInfo = this.getSlideInfo(slideHash);
 		const index = slideInfo ? slideInfo.index : undefined;
-		if (app.map.slideShowPresenter._isWelcomePresentation && index === 0 && window.mode.isCODesktop())
-			app.socket.sendMessage('SHOW_WELCOME');
+
+		if (
+			app.map.slideShowPresenter._isWelcomePresentation &&
+			index === 0 &&
+			window.mode.isCODesktop()
+		) {
+			const loader = document.getElementById('welcome-loader');
+			if (loader) {
+				loader.style.opacity = '0';
+				setTimeout(() => {
+					if (loader.parentNode) {
+						loader.parentNode.removeChild(loader);
+					}
+				}, 500);
+			}
+		}
 
 		if (!e.success) {
 			this.requestedSlideHash = null;
