c00b6f964e83840bc30979b2fc7ef51fd9dc5469
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-11-17 10:14:32 +0000

    add an exchange monitors button
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Iec261a7e2b25f76ab4dd982c0de30c8ae6536723

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -32,6 +32,7 @@ class PresenterConsole {
 			next: _('Next'),
 			notes: _('Notes'),
 			slides: _('Slides'),
+			exchange: _('Exchange'),
 			pause: _('Pause'),
 			restart: _('Restart'),
 			resume: _('Resume'),
@@ -96,6 +97,9 @@ class PresenterConsole {
 												</button>
 												<button type="button" id="slides" data-cooltip="${this.labels.slides}" aria-label="${this.labels.slides}">
 													<img src="${LOUtil.getImageURL('presenterscreen-ButtonSlideSorterNormal.svg')}">
+												</button>
+												<button type="button" id="exchange" data-cooltip="${this.labels.exchange}" aria-label="${this.labels.exchange}">
+													<img src="${LOUtil.getImageURL('presenterscreen-ButtonSwitchMonitorNormal.svg')}">
 												</button>
 											</div>
 										</div>
@@ -567,6 +571,13 @@ class PresenterConsole {
 			}.bind(this),
 		);
 
+		// By default hide the exchange monitors button
+		if (!window.mode.isCODesktop()) {
+				let exchangeMonitorsButton =
+					this._proxyPresenter.document.querySelector('#exchange');
+				exchangeMonitorsButton.style.display = 'none';
+		}
+
 		elem = this._proxyPresenter.document.querySelector('#today');
 		elem.style.textAlign = 'right';
 		elem.style.fontSize = '22px';
@@ -869,6 +880,9 @@ class PresenterConsole {
 			case 'slides':
 				this._onShowSlides();
 				break;
+			case 'exchange':
+				this._exchangeMonitors();
+				break;
 			case 'close-slides':
 				this._onHideSlides();
 				break;
@@ -959,6 +973,10 @@ class PresenterConsole {
 		closeSlideButton.style.display = 'none';
 
 		this._onResize();
+	}
+
+	_exchangeMonitors() {
+		window.postMobileMessage('EXCHANGEMONITORS');
 	}
 
 	_selectImg(img) {
