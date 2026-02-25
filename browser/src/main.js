CollaboraOnline/online

skip initializing UI component for welcome slideshow
- also stops the presentation on last slide

Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
Change-Id: Id30c6dbe9a6a80e2bda0a75edb77aaebe4cae799

browser/src/main.js
@@ -82,7 +82,10 @@ map.addControl(map.uiManager);
 if (!window.L.Browser.cypressTest)
 	map.tooltip = window.L.control.tooltip();
 
-map.uiManager.initializeBasicUI();
+if (window.coolParams.get('iswelcome'))
+	map.uiManager.initializeNonInteractiveUI();
+else
+	map.uiManager.initializeBasicUI();
 
 if (wopiSrc === '' && filePath === '' && !window.ThisIsAMobileApp) {
 	map.uiManager.showInfoModal('wrong-wopi-src-modal', '', errorMessages.wrongwopisrc, '', _('OK'), null, false);

grynov/collabora-online

browser/src/main.js
@@ -82,7 +82,10 @@ map.addControl(map.uiManager);
 if (!window.L.Browser.cypressTest)
 	map.tooltip = window.L.control.tooltip();
 
-map.uiManager.initializeBasicUI();
+if (window.coolParams.get('welcome'))
+	map.uiManager.initializeNonInteractiveUI();
+else
+	map.uiManager.initializeBasicUI();
 
 if (wopiSrc === '' && filePath === '' && !window.ThisIsAMobileApp) {
 	map.uiManager.showInfoModal('wrong-wopi-src-modal', '', errorMessages.wrongwopisrc, '', _('OK'), null, false);